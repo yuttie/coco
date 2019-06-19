611c5382aef2ba8127b1665cd847abaaa77679b7
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2019-06-19 19:08:19 +0900

    Refactoring

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import os
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+import flask_socketio as socketio
 
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
-socketio = SocketIO(app)
+socket = socketio.SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('orientation')
+@socket.on('orientation')
 def handle_orientation(json, methods=['GET', 'POST']):
     print(str(json))
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=8888, certfile='cert.pem', keyfile='key.pem')
+    socket.run(app, debug=True, host='0.0.0.0', port=8888, certfile='cert.pem', keyfile='key.pem')
